b49c0e9e6434403c943ccccac44c2e6455cf8efa
priyank awasthi <priyankawasthi252@gmail.com>
2021-05-23 17:29:18 +0530

    hello

---
+++ ast.java
@@ -0,0 +1,24 @@
+abstract class g
+{
+abstract void add();
+void print()
+{
+System.out.println("hello");
+}
+}
+public class h extends g
+{
+void add()
+{
+System.out.println("world");
+}
+}
+class good
+{
+public static void main(String[] args)
+{
+h h1=new h();
+h1.print();
+h1.add();
+}
+}
